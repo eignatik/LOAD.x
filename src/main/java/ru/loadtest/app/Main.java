3d1c9dcf34e69d3d63794ae8d2f0952d4fe95035
eignatik <light.ji@yandex.ru>
2016-10-25 15:45:30 -0400

    Refactored conection logic and moved to new package into the new class

--- src/main/java/ru/loadtest/app/Main.java
+++ src/main/java/ru/loadtest/app/Main.java
@@ -2,14 +2,19 @@ package ru.loadtest.app;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import ru.loadtest.app.HTTPConnection.ConnectionAPI;
+import ru.loadtest.app.HTTPConnection.HTTPConnection;
 import ru.loadtest.app.Pasrser.Parser;
+
+import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static final Logger logger = LogManager.getLogger(Main.class.getName());
 
     public static void main(String[] args){
-        HTTPConnection connection = HTTPConnection.getInstance();
-        Parser parser = new Parser();
-        parser.parseLinks(connection.getHTMLPageByURL("http://google.com/"));
+        ConnectionAPI connection = new ConnectionAPI();
+        connection.setBaseURL("http://yandex.ru");
+        connection.exploreLinks("");
     }
 }
