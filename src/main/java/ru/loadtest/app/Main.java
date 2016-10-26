56a2edd7f639bcf46e7e0fe218c12d6e6c7841a7
eignatik <light.ji@yandex.ru>
2016-10-26 12:18:47 -0400

    Deleted unecessary imports

--- src/main/java/ru/loadtest/app/Main.java
+++ src/main/java/ru/loadtest/app/Main.java
@@ -2,19 +2,14 @@ package ru.loadtest.app;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import ru.loadtest.app.HTTPConnection.ConnectionAPI;
-import ru.loadtest.app.HTTPConnection.HTTPConnection;
-import ru.loadtest.app.Pasrser.Parser;
-
-import java.util.List;
-import java.util.Random;
+import ru.loadtest.app.LoadTest.HTTPConnection.ConnectionAPI;
 
 public class Main {
     public static final Logger logger = LogManager.getLogger(Main.class.getName());
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ConnectionAPI connection = new ConnectionAPI();
-        connection.setBaseURL("http://yandex.ru");
+        connection.setBaseURL("http://mail.ru");
         connection.exploreLinks("");
     }
 }
