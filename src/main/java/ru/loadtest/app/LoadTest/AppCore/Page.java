1450b02c2fce2dddca355d096027eaf50437388a
eignatik <light.ji@yandex.ru>
2016-11-07 19:33:12 -0500

    Added Page class; maven dependency for apache commons

---
+++ src/main/java/ru/loadtest/app/LoadTest/AppCore/Page.java
@@ -0,0 +1,46 @@
+package ru.loadtest.app.LoadTest.AppCore;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import java.util.List;
+
+public class Page {
+    private String URL;
+    private List<String> links;
+
+    public Page(String URL, List<String> links){
+        this.URL = URL;
+        this.links = links;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public List<String> getLinks() {
+        return links;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Page page = (Page) o;
+
+        return new EqualsBuilder()
+                .append(URL, page.URL)
+                .append(links, page.links)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(URL)
+                .append(links)
+                .toHashCode();
+    }
+}
