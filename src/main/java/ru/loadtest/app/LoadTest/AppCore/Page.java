630d42fea81b2cea3e971393ca23cf049bdb0773
evgeny <evg.ignatik@gmail.com>
2016-11-21 12:16:01 +0300

    Added new fields and methods in Page for statistic Fields for calculating requests values, methods to operate with values

--- src/main/java/ru/loadtest/app/LoadTest/AppCore/Page.java
+++ src/main/java/ru/loadtest/app/LoadTest/AppCore/Page.java
@@ -3,11 +3,22 @@ package ru.loadtest.app.LoadTest.AppCore;
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Page {
     private String URL;
     private List<String> links;
+    private List<Integer> requests;
+
+    private int requestCount = requests.size();
+    private int maxRequest = getMaxValue();
+    private int minRequest = getMinValue();
+    private int avgRequest = getAverageValue();
+
+    private Random random;
+
 
     Page(String URL) {
         this.URL = URL;
@@ -16,6 +27,36 @@ public class Page {
     Page(String URL, List<String> links) {
         this.URL = URL;
         this.links = links;
+        requests = new ArrayList<>();
+        random = new Random();
+    }
+
+    private int getMaxValue() {
+        int max = 0;
+        for (Integer elem : requests) {
+            if(elem >= max) {
+                max = elem;
+            }
+        }
+        return max;
+    }
+
+    private int getMinValue() {
+        int min = 0;
+        for (Integer elem : requests) {
+            if(elem <= min) {
+                min = elem;
+            }
+        }
+        return min;
+    }
+
+    private int getAverageValue() {
+        int sum = 0;
+        for (Integer elem : requests) {
+            sum += elem;
+        }
+        return sum/requests.size();
     }
 
     public String getURL() {
@@ -27,6 +68,11 @@ public class Page {
     }
 
     public String getLinkByIndex(int index) {
+        return links.get(index);
+    }
+
+    public String getRandomLink() {
+        int index = random.nextInt(links.size()-1);
         return links.get(index);
     }
 
@@ -50,5 +96,41 @@ public class Page {
                 .append(URL)
                 .append(links)
                 .toHashCode();
+    }
+
+    public List<Integer> getRequests() {
+        return requests;
+    }
+
+    /**
+     *
+     * @return get requests count
+     */
+    public int getRequestCount() {
+        return requestCount;
+    }
+
+    /**
+     * get max request value in ms
+     * @return
+     */
+    public int getMaxRequest() {
+        return maxRequest;
+    }
+
+    /**
+     * get min request value in ms
+     * @return
+     */
+    public int getMinRequest() {
+        return minRequest;
+    }
+
+    /**
+     * get average request value in ms
+     * @return
+     */
+    public int getAvgRequest() {
+        return avgRequest;
     }
 }
