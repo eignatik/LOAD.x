a869843d719b086c5daca09107f84ba608811b04
evgeny <evg.ignatik@gmail.com>
2016-11-21 22:58:39 +0300

    refactor LoadtestAPI, added overloaded methods case

--- src/main/java/ru/loadtest/app/LoadTest/LoadTestAPI.java
+++ src/main/java/ru/loadtest/app/LoadTest/LoadTestAPI.java
@@ -30,31 +30,38 @@ public class LoadTestAPI {
 
     /**
      * Execute test with random visits of pages.
-     *
+     * Timeout for exploring is standart value that referenced in Core
      * @param startURL This is the first point of exploring. Explore is started from address.zone if @param startURL is empty string.
      */
     public void executeRandomTest(String startURL) {
         ConnectionAPI connection = new ConnectionAPI(this.URL, startURL);
         connection.start();
     }
+    /**
+     * Overloaded method with manual timeout
+     * @param startURL This is th first point of exploring. Explore is started from address.zone if @param startUrl is empty string
+     * @param timeout values in seconds that set timeout for testing time
+     */
+    public void executeRandomTest(String startURL, long timeout) {
+        ConnectionAPI.setTimeout(timeout);
+        ConnectionAPI connection = new ConnectionAPI(this.URL, startURL);
+        connection.start();
+    }
 
     /**
-     * Overloaded method with manual timeout value
+     * Overloaded method with manual timeout value and users count
      * @param startURL This is the first point of exploring. Explore is started from address.zone if @param startURL is empty string.
      * @param timeout value in seconds that set timeout for testing time
      */
     public void executeRandomTest(String startURL, long timeout, int usersCount) {
-
         List<ConnectionAPI> threads = new ArrayList<>();
         ConnectionAPI.setTimeout(timeout);
-//        ConnectionAPI connection = new ConnectionAPI(this.URL, startURL);
         for (int i = 0; i < usersCount; i++) {
             threads.add(new ConnectionAPI(this.URL, startURL));
         }
         for (ConnectionAPI thread : threads) {
             thread.start();
         }
-//        connection.start();
     }
 
     /**
