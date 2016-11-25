0622e8bd128898e87f4dc9c1f9c6ab57b5fade0c
eignatik <evg.ignatik@gmail.com>
2016-11-25 17:53:23 +0300

    Added queue and modified selection algorithm with queue, refactor small features

--- src/main/java/ru/loadtest/app/LoadTest/AppCore/Link.java
+++ src/main/java/ru/loadtest/app/LoadTest/AppCore/Link.java
@@ -16,6 +16,10 @@ public class Link {
         return this.reqCount;
     }
 
+    void incrementReqCount() {
+        this.reqCount ++;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/main/java/ru/loadtest/app/LoadTest/AppCore/Page.java
+++ src/main/java/ru/loadtest/app/LoadTest/AppCore/Page.java
@@ -3,13 +3,13 @@ package ru.loadtest.app.LoadTest.AppCore;
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class Page {
     private String URL;
     private List<Link> links;
+    private Queue<Link> linksQueue;
     private long requestsTimeSum;
     private long requestCount;
     private long maxRequest;
@@ -25,6 +25,10 @@ public class Page {
     Page(String URL, List<Link> links) {
         this.URL = URL;
         this.links = links;
+        if (links.size() == 0) {
+            links.add(new Link(""));
+        }
+        this.linksQueue = new PriorityBlockingQueue<>(links.size(), requestCountComparator);
     }
 
     public String getURL() {
@@ -39,11 +43,59 @@ public class Page {
         return links.get(index).getURL();
     }
 
-    public String getRandomLink() {
+    /**
+     * method generates 0 or 1 to select link generating way. If value 0 then random link from all list will be returned, else the random link from smallest request count list will be returned. Using PriorityQueue to sort links by request count value.
+     * @return
+     */
+    String getRandomLink() {
+        int direction = random.nextInt(3);
+        return getLink(direction);
+    }
+
+    private String getLink(int direction) {
+        switch (direction) {
+            case 0: return getRandomLinkFromList(links);
+            case 1:
+            case 2: return getQueuedRandomLink();
+            default: return getRandomLinkFromList(links);
+        }
+    }
+
+    private String getQueuedRandomLink() {
+        List<Link> linksWithSmallestReqCount = new ArrayList<>();
+        fillQueue();
+        linksWithSmallestReqCount = getLinksWithSmallestReqCount(linksWithSmallestReqCount);
+        return linksWithSmallestReqCount.isEmpty()? getRandomLinkFromList(links) : getRandomLinkFromList(linksWithSmallestReqCount);
+    }
+
+    private void fillQueue() {
+        for(Link link : links) {
+            linksQueue.add(link);
+        }
+    }
+
+    private List<Link> getLinksWithSmallestReqCount(List<Link> list) {
+        Link link = linksQueue.peek();
+        int smallestRequestCount = link.getReqCount();
+        boolean isRelevant = true;
+        while (isRelevant) {
+            if (link != null && link.getReqCount() == smallestRequestCount) {
+                list.add(link);
+            } else {
+                isRelevant = false;
+            }
+            link = linksQueue.poll();
+        }
+        linksQueue.clear();
+        return list;
+    }
+
+    private String getRandomLinkFromList(List<Link> links) {
         if(links.size() == 0) {
             return "";
         }
-        int index = (links.size() == 1)? 1 : random.nextInt(links.size()-1);
+        int index = (links.size() == 1)? 0 : random.nextInt(links.size()-1);
+        links.get(index).incrementReqCount();
         return links.get(index).getURL();
     }
 
@@ -127,6 +179,6 @@ public class Page {
         avgRequest = requestsTimeSum / requestCount;
     }
 
-
+    public static Comparator<Link> requestCountComparator = (o1, o2) -> (o1.getReqCount() - o2.getReqCount());
 
 }
