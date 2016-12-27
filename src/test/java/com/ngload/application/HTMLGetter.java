5a9167fd90908955b76607acde05c8293b9c6055
eignatik <evg.ignatik@gmail.com>
2016-12-27 16:43:15 +0300

    Added basic test for WebConector with built-in server (jetty) using Spark, added HTMLGetter, removed old tests and resources

---
+++ src/test/java/com/ngload/application/HTMLGetter.java
@@ -0,0 +1,34 @@
+package com.ngload.application;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class HTMLGetter {
+    public static final Logger logger = LogManager.getLogger(HTMLGetter.class.getName());
+    private ClassLoader loader = this.getClass().getClassLoader();
+
+    public String getBasicHTML() {
+        return getHTMLFromResourceURL(loader.getResource("html/test.html"));
+    }
+
+    private String getHTMLFromResourceURL(URL resource) {
+        StringBuilder HTML = new StringBuilder();
+        try {
+            Scanner input = new Scanner(new File(resource.getFile()));
+            while (input.hasNext()) {
+                HTML.append(input.nextLine());
+            }
+            input.close();
+        } catch (FileNotFoundException e) {
+            logger.error(e);
+        } catch (Exception e) {
+            logger.error(e);
+        }
+        return HTML.toString();
+    }
+}
---
+++ src/test/java/com/ngload/application/appcore/webcore/WebConnectorTest.java
@@ -0,0 +1,38 @@
+package com.ngload.application.appcore.webcore;
+
+import com.ngload.application.HTMLGetter;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static spark.Spark.*;
+
+public class WebConnectorTest {
+    private WebConnector connector;
+    private HTMLGetter htmlGetter = new HTMLGetter();
+
+    @Before
+    public void createTestConnection() {
+        port(8082);
+        get("/test", (req, res) -> htmlGetter.getBasicHTML());
+        connector = new WebConnector("http://localhost:8082");
+    }
+
+    @Test
+    public void getHtmlByURLSimpleCorrectPath() {
+        String html = connector.getHtmlByURL("/test");
+        assertEquals(html, htmlGetter.getBasicHTML());
+    }
+
+    @Test
+    public void getHtmlByURLSimplePathWithoutSlash() {
+        String html = connector.getHtmlByURL("test");
+        assertEquals(html, htmlGetter.getBasicHTML());
+    }
+
+    @Test
+    public void getHtmlByURLFullPath() {
+        String html = connector.getHtmlByURL("http://localhost:8082/test");
+        assertEquals(html, htmlGetter.getBasicHTML());
+    }
+}
