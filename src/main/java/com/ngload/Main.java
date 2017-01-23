c2dbd3873ef7b67c0bc5ee920298ddd94f63e370
eignatik <evg.ignatik@gmail.com>
2017-01-23 17:51:20 +0300

    Added structure and basic mulltythreading features for Parser

--- src/main/java/com/ngload/Main.java
+++ src/main/java/com/ngload/Main.java
@@ -1,9 +1,9 @@
 package com.ngload;
 
-import com.ngload.application.appcore.webcore.WebConnector;
+import com.ngload.application.appcore.webcore.Parser;
 
 public class Main {
     public static void main(String[] args) {
-
+        Parser.execute(60000);
     }
 }
--- src/main/java/com/ngload/application/appcore/webcore/Parser.java
+++ src/main/java/com/ngload/application/appcore/webcore/Parser.java
@@ -15,22 +15,45 @@ import java.util.Random;
 public class Parser extends Thread {
     public static final Logger logger = LogManager.getLogger(Parser.class.getName());
     private static WebConnector connection;
-    private static String workURL;
     private static final long DEFAULT_TIMEOUT = 60000;
+    private static long USER_TIMEOUT;
+
+    public static void execute(long timeout) {
+        int parsersCount = getParsersCount(timeout);
+        initThreads(parsersCount);
+        logger.info(parsersCount + " threads for Parser have been created and started");
+    }
+
+    private static void initThreads(int parsersCount) {
+        List<Parser> parsers = new ArrayList<>();
+        for (int i = 0; i < parsersCount; i++) {
+            parsers.add(new Parser());
+        }
+        parsers.parallelStream().forEach(Thread::start);
+    }
+
+    private static int getParsersCount(long timeout) {
+        long TIME = (timeout <= 0)? DEFAULT_TIMEOUT:timeout;
+        return (TIME <= 10000)? 1:(int)TIME/10000;
+    }
 
     @Override
     public void run() {
-
     }
 
     private void collectRandomLinks() {
+        long startTime = System.currentTimeMillis();
+        long currentTime = 0;
 
+        while (currentTime < DEFAULT_TIMEOUT) {
+            currentTime = System.currentTimeMillis() - startTime;
+        }
     }
 
     public static List<Link> getLinksFromURL(String URL) {
         List<Link> links = new ArrayList<>();
         Elements elements = selectElements(URL, "a[href]");
-        elements.stream().forEach((Parser) -> addLink(links, elements.attr("href")));
+        elements.forEach((Parser) -> addLink(links, elements.attr("href")));
         return links;
     }
 
