3d342ebbd8a224f599637443d9e8952d799058f2
eignatik <evg.ignatik@gmail.com>
2017-10-09 13:40:57 +0300

    Added duration test

--- src/main/java/com/ngload/application/appcore/util/Duration.java
+++ src/main/java/com/ngload/application/appcore/util/Duration.java
@@ -1,9 +1,13 @@
 package com.ngload.application.appcore.util;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Provides a few methods to operate with test durations
  */
 public class Duration {
+    private static final Logger logger = LogManager.getLogger(Duration.class);
     private int duration;
 
     /**
@@ -11,12 +15,14 @@ public class Duration {
      * @param duration must be in HH:MM:SS format
      */
     public Duration(String duration) {
-
+        this.duration = parseDurationToInt(duration);
     }
 
     private int parseDurationToInt(String duration) {
         if (duration.matches("\\d{2}\\:\\d{2}\\:\\d[0-5]{1}\\d[0-9]{1}")) {
-
+            String message = "Bad duration has been set" + duration;
+            logger.error(message);
+            throw new RuntimeException(message);
         }
         return 0;
     }
