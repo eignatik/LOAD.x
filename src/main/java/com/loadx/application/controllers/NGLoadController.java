78ed2f292d73a48871b2bc6307f5fa5748b8708d
eignatik <evg.ignatik@gmail.com>
2019-01-20 08:56:18 +0100

    #52 Reformat code

--- src/main/java/com/loadx/application/controllers/NGLoadController.java
+++ src/main/java/com/loadx/application/controllers/NGLoadController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 /**
  * Basic controller that provides the possibility to trigger execution jobs.
- *
+ * <p>
  * There are two way to trigger the job.
  * 1. Trigger /start endpoint to run job with default configuration
  */
