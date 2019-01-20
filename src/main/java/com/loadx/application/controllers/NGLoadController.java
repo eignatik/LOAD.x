f457fcd80864efe6ea9eaab3604a79b53806f3b2
eignatik <evg.ignatik@gmail.com>
2019-01-20 08:48:15 +0100

    #52 Fixes sonar issues

--- src/main/java/com/loadx/application/controllers/NGLoadController.java
+++ src/main/java/com/loadx/application/controllers/NGLoadController.java
@@ -2,23 +2,19 @@ package com.loadx.application.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Basic controller that provides the possibility to trigger execution jobs.
  *
  * There are two way to trigger the job.
  * 1. Trigger /start endpoint to run job with default configuration
- * 2. TODO: mapping for configurable job execution
  */
 
 @RestController
 public class NGLoadController {
 
-    @RequestMapping(value = "/start", method = RequestMethod.GET)
+    @GetMapping("/start")
     @ResponseBody
     public ResponseEntity triggerExecution() {
         return new ResponseEntity(HttpStatus.OK);
