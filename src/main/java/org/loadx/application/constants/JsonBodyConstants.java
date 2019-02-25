7f899c5a5d0871b2a5642c725a52b16e75798608
eignatik <evg.ignatik@gmail.com>
2019-02-25 21:37:38 +0100

    #45 adds a few tools dependencies, endpoint for generating the md5 hash for particular site, tests

---
+++ src/main/java/org/loadx/application/constants/JsonBodyConstants.java
@@ -0,0 +1,13 @@
+package org.loadx.application.constants;
+
+public final class JsonBodyConstants {
+
+    /**
+     * URL that should be passed for generating the hash
+     */
+    public static final String URL = "url";
+
+    private JsonBodyConstants() {
+        // hidden constructor for utility class
+    }
+}
--- src/main/java/org/loadx/application/controllers/NGLoadController.java
+++ src/main/java/org/loadx/application/controllers/NGLoadController.java
@@ -1,8 +1,13 @@
 package org.loadx.application.controllers;
 
+import org.apache.commons.lang3.StringUtils;
+import org.loadx.application.constants.JsonBodyConstants;
+import org.loadx.application.http.WebsiteValidationUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * Basic controller that provides the possibility to trigger execution jobs.
@@ -18,4 +23,15 @@ public class NGLoadController {
     public @ResponseBody ResponseEntity execute() {
         return new ResponseEntity(HttpStatus.OK);
     }
+
+    @GetMapping("/hashgen")
+    public ResponseEntity generateHashForOwnership(@RequestBody Map<String, String> payload) {
+        if (payload == null || StringUtils.isEmpty(payload.get(JsonBodyConstants.URL))) {
+            return ResponseEntity.badRequest()
+                    .body("The url should be present, but wasn't passed or passed empty");
+        }
+        return ResponseEntity.ok()
+                .body(WebsiteValidationUtil.generateHash(payload.get(JsonBodyConstants.URL)));
+    }
+
 }
