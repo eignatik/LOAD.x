5ff97b760faca5b528bc351f5088478543ae83ba
eignatik <evg.ignatik@gmail.com>
2019-04-14 17:20:11 +0300

    #49 updates constants

---
+++ src/main/java/org/loadx/application/constants/LoadTaskFields.java
@@ -0,0 +1,24 @@
+package org.loadx.application.constants;
+
+public enum LoadTaskFields {
+    BASE_URL("baseUrl", true),
+    LOADING_TIME("loadingTime", true),
+    PARALLEL_THRESHOLD("parallelRequestsThreshold", false),
+    REQUESTS("requests", true);
+
+    private String value;
+    private boolean required;
+
+    LoadTaskFields(String value, boolean required) {
+        this.value = value;
+        this.required = required;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+}
