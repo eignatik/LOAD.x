a4179fde80445483aea1dde375262ac8b2d2816f
eignatik <evg.ignatik@gmail.com>
2019-04-14 23:36:59 +0300

    #60 adds dummy stubbed implementation for loading task

--- src/main/java/org/loadx/application/processor/tasks/LoadingTask.java
+++ src/main/java/org/loadx/application/processor/tasks/LoadingTask.java
@@ -1,24 +1,34 @@
 package org.loadx.application.processor.tasks;
 
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.loadx.application.db.LoadPersistent;
+import org.loadx.application.db.entity.ExecutionDetails;
 import org.loadx.application.db.entity.LoadRequest;
 import org.loadx.application.db.entity.LoadTask;
+import org.loadx.application.db.entity.LoadingExecution;
+import org.loadx.application.http.WebsitesHttpConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 class LoadingTask implements Task<Integer> {
 
+    private static final Logger LOG = LoggerFactory.getLogger(LoadingTask.class);
+
     private LoadPersistent persistent;
     private LoadTask loadTask;
     private List<LoadRequest> loadRequests;
+    private WebsitesHttpConnector connector;
 
     public static LoadingTaskBuilder create() {
         return new LoadingTaskBuilder();
     }
 
     /**
-     *
      * @return submitted execution id
      */
     @Override
@@ -30,6 +40,39 @@ class LoadingTask implements Task<Integer> {
         // submit loading requests in thread pool etc
 //        CompletableFuture
 
+        // get pool capacity in runtime (if no available threads, wait don't create new requests
+        int i = 0;
+        while (true) {
+            LoadRequest loadRequest = loadRequests.get(i);
+            // check pool and wait before submitting
+            // put something to the queue and check  this queue for threshold
+            HttpUriRequest request = new HttpGet(loadTask.getBaseUrl() + loadRequest.getUrl());
+            CompletableFuture
+                    .supplyAsync(() -> {
+                        try {
+                            return connector.getHttpClient().execute(request);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                            // todo: handle exception properly, with adding info to statistics/db, exceptionally
+                        }
+                    })
+                    .thenApply(res -> {
+                        ExecutionDetails details = new ExecutionDetails();
+                        // todo: fill that shit
+                        return details;
+                    })
+                    .thenRun(() -> {
+                        // todo: write to buffer
+                    })
+                    .exceptionally(v -> {
+                        LOG.error("You are fucked up");
+                        // todo: some shit happened, write it somewhere
+                        return null;
+                    });
+            // TEMP BREAK
+            break;
+            //check time
+        }
 
 
         CompletableFuture<Integer> future = new CompletableFuture<>();
@@ -57,6 +100,11 @@ class LoadingTask implements Task<Integer> {
 
         public LoadingTaskBuilder withLoadRequests(List<LoadRequest> loadRequests) {
             task.loadRequests = loadRequests;
+            return this;
+        }
+
+        public LoadingTaskBuilder withConnector(WebsitesHttpConnector connector) {
+            task.connector = connector;
             return this;
         }
 
